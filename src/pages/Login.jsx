459a02cb93ae7e3f52b97f1447859191d89b77f9
gemago1 <gemagomartinez09@gmail.com>
2025-12-05 10:18:43 -0600

    Estilos de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService } from "../services/dataService";
 import axios from "axios";
+import galaxia from "../image/galaxia.mp4";
+import LOGOFUNVALblan from "../image/LOGOFUNVALblan.png";
+
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -81,10 +84,20 @@ export default function Login() {
   }
 
   return (
-    <div className="h-screen bg-gray-200 flex items-center justify-center">
+    <div className="h-screen flex relativ items-center justify-center flex-col gap-30">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+      >
+        <source src={galaxia} type="video/mp4" />
+      </video>
+      <img src={LOGOFUNVALblan} alt="" className="w-2xl opacity-80 p-8" />
       <form
         onSubmit={enviarFormulario}
-        className="max-w-sm mx-auto bg-slate-900 p-8 rounded-2xl"
+        className="max-w-sm md:w-2xl mx-auto bg-slate-900 p-8 rounded-2xl opacity-90"
       >
         {error && (
           <div className="mb-5 p-3 bg-red-100 border border-red-400 text-red-700 rounded-lg">
@@ -104,7 +117,7 @@ export default function Login() {
             type="email"
             id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
+            placeholder="your.email@funval.com"
             required
             disabled={loading}
           />
@@ -122,6 +135,7 @@ export default function Login() {
             type="password"
             id="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="**********"
             required
             disabled={loading}
           />
@@ -147,20 +161,20 @@ export default function Login() {
         <div className="flex gap-4">
           <button
             type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-50 disabled:cursor-not-allowed"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-50 disabled:cursor-not-allowed"
             disabled={loading}
           >
             {loading ? "Cargando..." : "Submit"}
           </button>
 
-          <button
+          {/* <button
             onClick={crearCuenta}
             type="button"
             className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 disabled:opacity-50 disabled:cursor-not-allowed"
             disabled={loading}
           >
             crear cuenta
-          </button>
+          </button> */}
         </div>
       </form>
     </div>
